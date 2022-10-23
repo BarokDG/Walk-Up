3692d54382ace2e143e045e3a361e0f7d0968cf3
BarokDG <it.barok.dagim@gmail.com>
2022-10-23 19:57:16 +0300

    Add start button layout

--- assets/js/videoplay.js
+++ assets/js/videoplay.js
@@ -1,73 +1,74 @@
-video = document.querySelector('#v1');
-video2 = document.querySelector('#v2');
-video3 = document.querySelector('#v3');
-video4 = document.querySelector('#v4');
-video5 = document.querySelector('#v5');
-video6 = document.querySelector('#v6');
-video7 = document.querySelector('#v7');
-video8 = document.querySelector('#v8');
-video9 = document.querySelector('#v9');
-video10 = document.querySelector('#v10');
-video11 = document.querySelector('#v11');
-video12 = document.querySelector('#v12');
-video13 = document.querySelector('#v13');
-video14 = document.querySelector('#v14');
+video = document.querySelector("#v1");
+video2 = document.querySelector("#v2");
+video3 = document.querySelector("#v3");
+video4 = document.querySelector("#v4");
+video5 = document.querySelector("#v5");
+video6 = document.querySelector("#v6");
+video7 = document.querySelector("#v7");
+video8 = document.querySelector("#v8");
+video9 = document.querySelector("#v9");
+video10 = document.querySelector("#v10");
+video11 = document.querySelector("#v11");
+video12 = document.querySelector("#v12");
+video13 = document.querySelector("#v13");
+video14 = document.querySelector("#v14");
 
-
-setTimeout(function () {
+function startPlayback() {
+  setTimeout(function () {
     video.play();
-}, 8500)
+  }, 8500);
 
-setTimeout(function () {
+  setTimeout(function () {
     video2.play();
-}, 13200)
+  }, 13200);
 
-setTimeout(function () {
+  setTimeout(function () {
     video3.play();
-}, 13500)
+  }, 13500);
 
-setTimeout(function () {
+  setTimeout(function () {
     video4.play();
-}, 18500)
+  }, 18500);
 
-setTimeout(function () {
+  setTimeout(function () {
     video5.play();
-}, 23500)
+  }, 23500);
 
-setTimeout(function () {
+  setTimeout(function () {
     video6.play();
-}, 23500)
+  }, 23500);
 
-// setTimeout(function () {
-//     video7.play();
-// }, 37500)
+  // setTimeout(function () {
+  //     video7.play();
+  // }, 37500)
 
-// ================================================
+  // ================================================
 
-setTimeout(function () {
+  setTimeout(function () {
     video8.play();
-}, 43500)
+  }, 43500);
 
-setTimeout(function () {
+  setTimeout(function () {
     video9.play();
-}, 48500)
+  }, 48500);
 
-setTimeout(function () {
+  setTimeout(function () {
     video10.play();
-}, 48500)
+  }, 48500);
 
-// setTimeout(function () {
-//     video11.play();
-// }, 47500)
+  // setTimeout(function () {
+  //     video11.play();
+  // }, 47500)
 
-// setTimeout(function () {
-//     video12.play();
-// }, 52500)
+  // setTimeout(function () {
+  //     video12.play();
+  // }, 52500)
 
-setTimeout(function () {
+  setTimeout(function () {
     video13.play();
-}, 55500)
+  }, 55500);
 
-setTimeout(function () {
+  setTimeout(function () {
     video14.play();
-}, 60500)
+  }, 60500);
+}
